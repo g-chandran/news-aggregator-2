d874cfa213ae2a0d4c1b8d206c9945492b05dbbd
g-chandran <chandranchan321@gmail.com>
2021-06-01 14:09:14 +0530

    Created ORMs

--- aggregator/admin.py
+++ aggregator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Subscription, Article, Profile
 
-# Register your models here.
+admin.site.register(Subscription)
+admin.site.register(Article)
+admin.site.register(Profile)
